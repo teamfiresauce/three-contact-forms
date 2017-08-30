e49f5a3db55613b17c57fbe6bb0b6b7eda2f04c7
Edward Richards <e4.rich87@gmail.com>
2017-08-30 17:11:39 -0600

    added mail.php

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php 
+
+  $name = $_REQUEST['name'];
+  $from = $_REQUEST['from'];
+  $body = $_REQUEST['body'];
+
+  $to = "hiro@mrhiro.com"; //recipient 
+
+  $subject = "RIA Emails"; //subject 
+  $header = "From: ". $name . " <" . $from . ">\r\n";
+
+  if (mail($to, $subject, $body, $header)){
+    echo 'Your email has been sent!';
+  } else {
+    echo 'Error: something went wrong.';
+  }
+
+?>
